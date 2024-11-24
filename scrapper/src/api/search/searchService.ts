9f228139a0d9ccbe85b54b95cae1c530b92c7024
yoarajota <joaovbscontato@gmail.com>
2024-11-23 23:25:15 -0300

    feat: sendgrid

--- scrapper/src/api/search/searchService.ts
+++ scrapper/src/api/search/searchService.ts
@@ -15,115 +15,121 @@ export class SearchService {
         queue: "search-topic",
         jobData: { newsletter_topic_id },
         callback: async ([job]) => {
-          const db = getDrizzleDb()
+          try {
+            const db = getDrizzleDb()
 
-          const { newsletter_topic_id } = job.data
+            const { newsletter_topic_id } = job.data
 
-          const newsLetterTopicRegister = await db.query.newslettersTopics.findFirst({
-            where: (posts, { eq }) => eq(posts.id, newsletter_topic_id),
-          })
-
-          if (newsLetterTopicRegister) {
-            const { se_description, name, summary } = newsLetterTopicRegister
-
-            const browser = await puppeteer.launch()
-
-            const page = await browser.newPage()
-
-            // Navigate the page to a URL.
-            await page.goto(`https://www.google.com/search?q=${se_description}&tbm=nws`)
-
-            // await #search
-            await page.waitForSelector("#search")
-
-            const searchResult = await page.waitForFunction(() => {
-              const links = Array.from(document.querySelectorAll("#search a"))
-
-              return links.map((link) => link.getAttribute("href"))
+            const newsLetterTopicRegister = await db.query.newslettersTopics.findFirst({
+              where: (posts, { eq }) => eq(posts.id, newsletter_topic_id),
             })
 
-            // For now, get first 10 results
-            // const urls = (await searchResult.jsonValue()).slice(0, 10)
-            const urls = (await searchResult.jsonValue()).slice(0, 2)
+            if (newsLetterTopicRegister) {
+              const { se_description, name, summary } = newsLetterTopicRegister
 
-            const pages: { url: string; page: Page }[] = []
-            const promisesPages = []
-            for (const url of urls) {
-              if (url) {
-                promisesPages.push(
-                  browser.newPage().then(async (newPage) => {
-                    await newPage.goto("https://www.smry.ai/" + url)
-                    pages.push({
-                      page: newPage,
-                      url,
-                    })
-                  })
-                )
-              }
-            }
-
-            await Promise.all(promisesPages)
-
-            const toStore: {
-              url: string
-              content: string
-            }[] = []
-            for (const { page, url } of pages) {
-              // browser focus in the page
-              await page.bringToFront()
-
-              // get the page content
-              // data-state="active"
-              // data-orientation="horizontal"
-              // get what is inside the div with data-state="active" and data-orientation="horizontal"
-
-              await page.waitForSelector('div[data-state="active"][data-orientation="horizontal"]')
-
-              const content = await page.evaluate(() => {
-                const activeDiv = document.querySelector('div[data-state="active"][data-orientation="horizontal"]')
-
-                return activeDiv?.textContent
+              const browser = await puppeteer.launch({
+                headless: false,
               })
 
-              if (content) {
-                toStore.push({
-                  url,
-                  content,
+              const page = await browser.newPage()
+
+              // Navigate the page to a URL.
+              await page.goto(`https://www.google.com/search?q=${se_description}&tbm=nws`)
+
+              // await #search
+              await page.waitForSelector("#search")
+
+              const searchResult = await page.waitForFunction(() => {
+                const links = Array.from(document.querySelectorAll("#search a"))
+
+                return links.map((link) => link.getAttribute("href"))
+              })
+
+              // For now, get first 10 results
+              // const urls = (await searchResult.jsonValue()).slice(0, 10)
+              const urls = (await searchResult.jsonValue()).slice(0, 2)
+
+              const pages: { url: string; page: Page }[] = []
+              const promisesPages = []
+              for (const url of urls) {
+                if (url) {
+                  promisesPages.push(
+                    browser.newPage().then(async (newPage) => {
+                      await newPage.goto("https://www.smry.ai/" + url)
+                      pages.push({
+                        page: newPage,
+                        url,
+                      })
+                    })
+                  )
+                }
+              }
+
+              await Promise.all(promisesPages)
+
+              const toStore: {
+                url: string
+                content: string
+              }[] = []
+              for (const { page, url } of pages) {
+                // browser focus in the page
+                await page.bringToFront()
+
+                await page.waitForSelector('div[data-state="active"][data-orientation="horizontal"]')
+                await page.waitForSelector('div[data-state="active"][data-orientation="horizontal"] article h1', {
+                  timeout: 7500,
+                })
+
+                const content = (
+                  await page.evaluate(() => {
+                    const activeDiv = document.querySelector(
+                      'div[data-state="active"][data-orientation="horizontal"] article'
+                    )
+
+                    return activeDiv?.textContent
+                  })
+                )?.trim?.()
+
+                if (content && !content.includes(`No data from`)) {
+                  toStore.push({
+                    url,
+                    content: content,
+                  })
+                }
+              }
+
+              if (toStore.length > 0) {
+                const date = new Date()
+
+                const contents = toStore.map((content) => ({
+                  newsletter_topic_id,
+                  url: content.url,
+                  content: content.content,
+                  created_at: date,
+                }))
+
+                await db.insert(newslettersTopicFilesContents).values(contents).onConflictDoNothing()
+
+                handlePgBoss({
+                  queue: "generate-email",
+                  jobData: { newsletter_topic_id, date: date.toISOString() },
+                  callback: async ([job]) => {
+                    const supabase = createAdminClient()
+
+                    const { error } = await supabase.functions.invoke("generate-topic-email", {
+                      body: {
+                        newsletter_topic_id: job.data.newsletter_topic_id,
+                        date: job.data.date,
+                      },
+                    })
+                  },
                 })
               }
+
+              await browser.close()
             }
-
-            if (toStore.length > 0) {
-              const date = new Date()
-
-              const contents = toStore.map((content) => ({
-                newsletter_topic_id,
-                url: content.url,
-                content: content.content,
-                created_at: date,
-              }))
-
-              await db.insert(newslettersTopicFilesContents).values(contents).onConflictDoNothing()
-
-              handlePgBoss({
-                queue: "generate-email",
-                jobData: { newsletter_topic_id, date: date.toUTCString() },
-                callback: async ([job]) => {
-                  const supabase = createAdminClient()
-
-                  const { error } = await supabase.functions.invoke("generate-topic-email", {
-                    body: {
-                      newsletter_topic_id: job.data.newsletter_topic_id,
-                      date: job.data.date,
-                    },
-                  })
-
-                  console.log(error)
-                },
-              })
-            }
-
-            await browser.close()
+          } catch (error) {
+            logger.error(`Error searching for topic: ${(error as Error).message}`)
           }
         },
       })
--- supabase/supabase/functions/generate-topic-email/index.ts
+++ supabase/supabase/functions/generate-topic-email/index.ts
@@ -1,18 +1,62 @@
-import { env, pipeline } from "https://cdn.jsdelivr.net/npm/@xenova/transformers@2.5.0"
 import Groq from "https://cdn.jsdelivr.net/npm/groq-sdk@0.7.0/+esm"
+import { marked } from "https://cdn.jsdelivr.net/npm/marked@15.0.2/+esm"
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import { Tables } from "../_shared/db_types.ts"
-import { error, getUserAccount, success } from "../_shared/index.ts"
-import { createSupabaseAdminClient, createSupabaseClient } from "../_shared/supabase.ts"
+import { error, success } from "../_shared/index.ts"
+import { createSupabaseAdminClient } from "../_shared/supabase.ts"
+
+const BASE = `
+<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: transparent;">
+  <tbody>
+    <tr>
+      <td>
+        {CONTENT}
+      </td>
+    </tr>
+  </tbody>
+</table>
+`
+
+// const HEADER = `
+// <table class="heading_block" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+//   <tbody>
+//     <tr>
+//       <td class="pad">
+//         <h1 style="margin: 0; color: #1e0e4b; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 38px; font-weight: 700; line-height: 120%; text-align: left;">
+//           {TITLE}
+//         </h1>
+//       </td>
+//     </tr>
+//   </tbody>
+// </table>
+// `
+
+// const PARAGRAPH = `
+// <table class="paragraph_block" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;">
+//   <tbody>
+//     <tr>
+//       <td class="pad">
+//         <div style="color: #444a5b; font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; font-size: 16px; line-height: 120%; text-align: left;">
+//           <p style="margin: 0;">{PARAGRAPH}</p>
+//         </div>
+//       </td>
+//     </tr>
+//   </tbody>
+// </table>
+// `
 
 const groq = new Groq({ apiKey: Deno.env.get("GROQ_API_KEY") })
+
+// const template = `[{
+//   text: constr(str),
+//   type: constr(paragraph|header),
+// }]`
 
 const generator = async (content: string) => {
   const chatCompletion = await groq.chat.completions.create({
     messages: [
       {
         role: "system",
-        content: `You are a newsletter writer. You create rich content for newsletters. You receive a lot of information from web pages and you need to summarize them in a email. You write in Portuguese.`,
+        content: `You are a newsletter writer. You create rich content for newsletters. You receive a lot of information from web pages and you need to summarize them in a email. You write in Portuguese. You only returns markdown text.`,
       },
       {
         role: "user",
@@ -20,12 +64,12 @@ const generator = async (content: string) => {
       },
     ],
     model: "llama3-8b-8192",
-    temperature: 0.4,
+    temperature: 0.5,
     max_tokens: 1024,
     top_p: 1,
   })
 
-  return JSON.parse(chatCompletion.choices[0].message.content).msg
+  return chatCompletion.choices[0].message.content
 }
 
 // Deno Handler
@@ -33,23 +77,61 @@ serve(async (req) => {
   try {
     const { newsletter_topic_id, date } = await req.json()
 
-    if (!newsletter_topic_id || !date) {
-      throw new Error("Invalid request")
-    }
-
-    const dateObject = new Date(date)
+    // if (!newsletter_topic_id || !date) {
+    //   throw new Error("Invalid request")
+    // }
 
     const admin = createSupabaseAdminClient()
 
     const { data: contents, error } = await admin
       .from("newsletters_topic_files_contents")
-      .select("*")
-      .eq("id", newsletter_topic_id)
-      .eq("creatd_at", dateObject)
+      .select("content, url")
+      .eq("newsletter_topic_id", newsletter_topic_id)
+      .eq("created_at", date)
 
-    generator(JSON.stringify(contents.map(({ content }) => content)))
+    if (contents.length) {
+      // const a = await generator(JSON.stringify(contents.map(({ content }) => content)))
 
-    console.log("Email generated")
+      const response = await fetch("https://api.sendgrid.com/v3/mail/send", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${Deno.env.get("SENDGRID_API_KEY")}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          personalizations: [
+            {
+              to: [
+                {
+                  email: "joaovbscontato@gmail.com",
+                  name: "João",
+                },
+              ],
+              subject: "Hello, World!",
+            },
+          ],
+          content: [
+            {
+              type: "text/html",
+              value: BASE.replace("{CONTENT}", marked.parse(a.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, ""))),
+            },
+          ],
+          from: {
+            email: "joaovbscontato@gmail.com",
+            name: "João",
+          },
+        }),
+      })
+
+      console.log(response)
+
+      if (response.ok) {
+        console.log("Email enviado com sucesso!")
+      } else {
+        const error = await response.text()
+        console.error("Erro ao enviar email:", response.status, error)
+      }
+    }
 
     return success({
       message: "Email generated",
@@ -66,7 +148,7 @@ serve(async (req) => {
   1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
   2. Make an HTTP request:
 
-  curl -i --location --request POST 'http://127.0.0.1:64321/functions/v1/handle-topic' \
+  curl -i --location --request POST 'http://127.0.0.1:64321/functions/v1/generate-topic-email' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
